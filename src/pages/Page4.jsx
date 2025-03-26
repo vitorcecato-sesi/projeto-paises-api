d80604c853d0cd044408549d752c5ab1397190f0
Laura Betti <bettilaura0@gamail.com>
2025-03-26 15:43:04 -0300

    pagina 4

--- src/pages/Page4.jsx
+++ src/pages/Page4.jsx
@@ -1,31 +1,31 @@
 import { useState } from "react";
+import Navbar from "../components/Navbar";
 import "./styles/Page4.css"
 
 function Page4 () {
-    const [pais] = useState (() => JSON.parse(localStorage.getItem("Pais")) || "")
-    const [fronteira] = useState (() => JSON.parse(localStorage.getItem("Fronteiras")) || "")
-    const [fusoHorario] = useState (() => JSON.parse(localStorage.getItem("Fuso-Horário")) || "")
-    const [sigla] = useState (() => JSON.parse(localStorage.getItem("Sigla")) || "")
+    const [informacoes] = useState(JSON.parse(localStorage.getItem("Informações")) || "")
 
     return ( 
         <>
+        <Navbar />
+
         <section className="bodyPage4"> 
-            <h2 className="titulo"> {pais} Italia </h2>
+            <h2 className="titulo"> {informacoes.nomeComum} </h2>
 
             <section className="blocos">
                 <section className="bloco"> 
                     <h3> Paises que Fazem Fronteira: </h3>
-                    <h3> <strong> {fronteira} </strong> </h3>
+                    <h3> <strong> {informacoes.paisesFronteira} </strong> </h3>
                 </section>
 
                 <section className="bloco"> 
                     <h3> Fuso-Horário: </h3>
-                    <h3> <strong> {fusoHorario} </strong> </h3>
+                    <h3> <strong> {informacoes.fusoHorario} </strong> </h3>
                 </section>
 
                 <section className="bloco"> 
                     <h3> Sigla do País: </h3>
-                    <h3> <strong> {sigla} </strong> </h3>
+                    <h3> <strong> {informacoes.sigla} </strong> </h3>
                 </section>
             </section>
         </section>
