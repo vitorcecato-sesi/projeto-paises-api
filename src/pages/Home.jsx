79c679fdd9ed51303200cd090c6d5dfe562f0762
Extreme <npyetro26@gmail.com>
2025-03-26 14:25:41 -0300

    HomePageAtualuzada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,78 +1,65 @@
 import { useEffect, useState } from "react";
 
-
-
-
-function Pais(){
-const[Pais, setPais] = useState( []); // Array das mensagens selecionadas da API (seráo exibidas)
-    const [loading, setLoading] = useState(true); // Estado de carregamento da API
-    const [erro, setErro] = useState(null);
-    const [paisEscolhido, setPaisEscolhido] = useState(
-        ()=> JSON.parse(localStorage.getItem("Pais")) ||[]); // Array das mensagens selecionadas
-    
-
-    useEffect(() => {
-        async function fetchPais() { // Função que vai buscar os usuários
-          try {
-            const coutries = await fetch("https://restcountries.com/v3.1/all");
-            if (!coutries.ok) {
-              throw new Error("Erro ao carregar usuários");
-            }
-            const dados = await coutries.json(); // Vai converter as respostas em JSON
-            setPais(dados); // Salva as informações da API na nossa variável "mensagens"
-          } catch (erro) {
-            setErro(erro.coutries); // Se estiver algum erro, atualiza o estado do erro
-          } finally {
-            setLoading(false); //Atualiza o estado do carregamento para true quando carregar
-          }
-        }
-        //Chama a função de buscar usuários
-        fetchPais();
-      },[]);
-      useEffect(() => {
-        localStorage.setItem("paisEscolhido", JSON.stringify(paisEscolhido))
-    }, [paisEscolhido])
-    if (loading) return <h2>Carregando usuários... </h2>;
-    // Erro = veifica se houve algum erro
-    if (erro) return <h2>Erro: {erro}</h2>;
-
-
-
-return(
-<>
-<h1>
-Selecione o pais.
-
-
-</h1>
-
-   <select value={paisEscolhido} onChange={(e) => setPaisEscolhido(e.target.value)} > 
-   {Pais.map((coutries) => (
-   <option value={coutries.name.common}>{coutries.name.common}</option>
-   ))}
-
-   
-   </select>
-
- <h2>{paisEscolhido}</h2>   
+function Home() {
+  const [paises, setPaises] = useState([])
+  const [informacoes, setInformacoes] = useState(JSON.parse(localStorage.getItem("Informações")) || "")
   
- {Pais.map(pais => (
-            /* Caso o nome do país que for pego no .map for igual ao escolhido, pegue e mostre a imagem */
-          pais.name.common === paisEscolhido && (
-            <img src={pais.flags.png} alt={pais.name.common} />
-            // ".flag.png" é a URL da imagem da bandeira do país, pois o array com a imagem na API está com esse nome
-          )
-        ))}
+  useEffect(() => {
+    async function buscarPaises() {
+      try {
+        const resposta = await fetch("https://restcountries.com/v3.1/all")
+        const dados = await resposta.json()
+        setPaises(dados)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    buscarPaises()
+  }, [])
 
+  const guardarInformacoes = (pais) => {
+    const informacoes = {
+      
+      nomeComum: pais.name.common,
+      nomeOficial: pais.name.official,
+      lingua: pais.languages,
+      capital: pais.capital,
+      regiao: pais.region,
+      subRegiao: pais.subregion,
+      moeda: pais.currencies,
+      area: pais.area,
+      populacao: pais.population,
+      paisesFronteira: pais.borders,
+      fusoHorario: pais.timezones,
+      sigla: pais.flag,
+      bandeira: pais.flags.png
+    }
+    setInformacoes(informacoes)
+    localStorage.setItem("Informações", JSON.stringify(informacoes))
+  }
 
-</>
+  return (
+    <>
+    <h3> Selecione seu país: </h3>
 
+    <select value={informacoes?.nomeComum} onChange={(e) => guardarInformacoes(paises.find(pais => pais.name.common === e.target.value))}>
+      <option> Selecione um país </option>
 
+      {paises.map((pais) => (
+        <option key={pais.name.common}>{pais.name.common} </option>
+      ))}
+    </select>
 
+    {informacoes && (
+      <>
+      <p> Pais selecionado: {informacoes.nomeComum} </p>
+      <img src={informacoes.bandeira}/>
 
-)
-
-
+      </>
+    )}
+    </>
+  )
 
 }
-export default Pais
+
+export default Home
