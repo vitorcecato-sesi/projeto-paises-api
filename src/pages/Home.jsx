961a6d7285b2c2fab542f96d58d06604e540de8e
PietroMelle <mellepietro3@gmail.com>
2025-03-26 15:34:45 -0300

    add google maps home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ function Home() {
       regiao: pais.region,
       subRegiao: pais.subregion,
       moeda: pais.currencies ? pais.currencies[Object.keys(pais.currencies)[0]].name : "Moeda não conhecida!",
-      area: pais.area,
+      googleMaps: pais.maps.googleMaps,
       populacao: pais.population,
       paisesFronteira: pais.borders,
       fusoHorario: pais.timezones ? pais.timezones.join(", ") : "Fuso-Horário não conhecido!",
