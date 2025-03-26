903cd9f905cd3393b80e4ab6edf05808e1250a38
PietroMelle <mellepietro3@gmail.com>
2025-03-26 14:27:01 -0300

    inicio

---
+++ src/pages/Page1.jsx
@@ -0,0 +1,25 @@
+/*
+🚩INFORMAÇÕES PARA A PAGE:
+1️⃣ Nome comum (.name.cummon)
+2️⃣ Nome oficial (.name.official)
+3️⃣ Lingua oficial (.languages)
+*/
+
+// Estilização
+import "./styles/Page1.css"
+
+// Hooks
+import { useState, useEffect } from "react";
+
+function Page1Informacoes() {
+  
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default Page1Informacoes;
+
+
