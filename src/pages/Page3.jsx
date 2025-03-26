a076682befa658cd73231ced89e99186ab42045b
Lucas C <lucascasagrandesesi@gmail.com>
2025-03-26 14:39:45 -0300

    Preparação para fazer a page3

---
+++ src/pages/Page3.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import "./styles/Page3.css"
+
+
+function PageTres() {
+
+    return (
+        <>
+        <i class="fa-solid fa-users"></i>
+        <i class="fa-solid fa-coins"></i>
+        <i class="fa-solid fa-map-location-dot"></i>
+        </>
+    )
+}
+
+export default PageTres
