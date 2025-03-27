53e8869478943317ee85d5d55a3c1d0dae548d30
Milena <mile.26souza08@gmail.com>
2025-03-27 08:28:15 -0300

    tudo estilizado

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -2,34 +2,66 @@ import { useState } from "react";
 import "./styles/page2.css"
 
 function RouteLocalStorage() {
-    const [capital] = useState (() => JSON.parse(localStorage.getItem("Capital")) || "")
-    const [regiao] = useState (() => JSON.parse(localStorage.getItem("Regiao")) || "")
-    const [subRegiao] = useState (() => JSON.parse(localStorage.getItem("Sub-Regiao")) || "")
+    const [informacao] = useState (() => JSON.parse(localStorage.getItem("Informações")) || "")
 
     return ( 
+        <div>
+            <div className="ti-img">
+        <h1 className="Ti">{informacao.nomeComum}</h1>
+        <img src={informacao.bandeira}></img>
+        </div>
         <section className="Box-Tudo" >
 
+           
+ 
             <section className="Blocao">
 
+            
+              
+                <div>
+              <h3 className="Ti-Informações">Algumas informacões sobre "{informacao.nomeComum}"</h3>
+              </div>
+
+              <div className="Bloc">
+            <div className="Blox">
             <section className="Capital">
+                <center>
             <h3 className="capTexto">Capital</h3>
-            <p> {capital}</p>
+            </center>
             </section>
+            <p> {informacao.capital}</p>
+            </div>
 
+            <br/>
+
+            <div className="Blox">
             <section className="Regiao">
+                <center>
             <h3>Região</h3>
-            <p> {regiao}</p>
+            </center>
             </section>
+            <p> {informacao.regiao}</p>
+            </div>
 
+            <br/>
+
+            <div className="Blox">
             <section className="Sub-Regiao">
+            <center>
             <h3>Sub-Região</h3>
-            <p> {subRegiao}</p>
+            </center>
             </section>
-
+            <p> {informacao.subRegiao}</p>
+            </div>
+            </div>
+         
+            
             </section>
+           
 
 
         </section>
+        </div>
     )
 }
 
