8fd32beb69cc1aacfc2b0e227dcc0f090ef0ba96
Laura Betti <bettilaura0@gamail.com>
2025-03-27 09:47:18 -0300

    footer com as novas informações

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,18 +14,18 @@ function Footer() {
 
             <section className='bloco-img1'>
             <img className='img-redes' src={foto}/>
-            <h3 className='email' >vitor.cecatosesi@gmail.com</h3>
+            <h3 className='email' >abacate.grupo3@gmail.com</h3>
             </section>
 
             <section className='bloco-img2'>
             <img className='img-redes' src={foto2}/>
-            <h3 className='instagram'>@vitorgcecato</h3>
+            <h3 className='instagram'>@abacates_3</h3>
             </section>
 
         </section>
 
         <section className="informacao-resposavel" >
-            <p>O Vitor Geraldo Cecato é o responsável por este projeto, cuidando da coordenação e garantindo que o conteúdo seja de qualidade e esteja sempre no caminho certo.</p>
+            <p>Laura Betti Migliaccio - Lucas Casagrande da Silva - Milena Oliveira Souza <br/> Pietro Melle Michelin - Pyetro Joaquim Nunes - Vitor Geraldo Cecato </p>
             <img className='imglogo' src={logo}/>
         </section>
 
