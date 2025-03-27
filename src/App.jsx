82d69ad31b42477124408347aa234c21ce17e6ee
Milena <mile.26souza08@gmail.com>
2025-03-27 09:48:39 -0300

    coloquei minha pagina no app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Page1 from "./pages/Page1"
 import Page4 from "./pages/Page4"
 import PageTres from './pages/Page3'
+import Page2 from "./pages/Page2"
 
 import './App.css'
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/page4" element={<Page4 />} /> 
           <Route path="/page3" element={<PageTres />} /> 
           <Route path="/page1" element={<Page1 />} /> 
+          <Route path="/page2" element={<Page2 />}/>
         </Routes>
       </BrowserRouter>
     </> 
