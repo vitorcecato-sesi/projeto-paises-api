c0112da171e11c1fcb9f83f4e74a150a368b6629
Milena <mile.26souza08@gmail.com>
2025-03-27 09:44:48 -0300

    Finalizado

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,68 +1,76 @@
 import { useState } from "react";
-import "./styles/page2.css"
+import "./styles/page2.css";
+
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function RouteLocalStorage() {
-    const [informacao] = useState (() => JSON.parse(localStorage.getItem("Informações")) || "")
+  const [informacao] = useState(
+    () => JSON.parse(localStorage.getItem("Informações")) || ""
+  );
 
-    return ( 
-        <div>
-            <div className="ti-img">
-        <h1 className="Ti">{informacao.nomeComum}</h1>
-        <img src={informacao.bandeira}></img>
-        </div>
-        <section className="Box-Tudo" >
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <div className="nome-Pais">
+        <h1 className="Titulo-Pais">{informacao.nomeComum}</h1>
+      </div>
 
-           
- 
-            <section className="Blocao">
+      <section className="Box-Informacoes-Bandeira">
+        <section className="Box-Informacoes">
+          <div>
+            <h3 className="Titulo-Informações">
+              Algumas informacões sobre "{informacao.nomeComum}"
+            </h3>
+          </div>
 
-            
-              
-                <div>
-              <h3 className="Ti-Informações">Algumas informacões sobre "{informacao.nomeComum}"</h3>
-              </div>
+          <div className="Bloc-Informacoes-linha">
 
-              <div className="Bloc">
-            <div className="Blox">
-            <section className="Capital">
+            <div className="Blox-Informacoes">
+              <section className="Capital-Titulo">
                 <center>
-            <h3 className="capTexto">Capital</h3>
-            </center>
-            </section>
-            <p> {informacao.capital}</p>
+                  <h4>Capital</h4>
+                </center>
+              </section>
+              <p> {informacao.capital}</p>
             </div>
 
-            <br/>
+            <br />
 
-            <div className="Blox">
-            <section className="Regiao">
+            <div className="Blox-Informacoes">
+              <section className="Regiao-Titulo">
                 <center>
-            <h3>Região</h3>
-            </center>
-            </section>
-            <p> {informacao.regiao}</p>
+                  <h4>Região</h4>
+                </center>
+              </section>
+              <p> {informacao.regiao}</p>
             </div>
 
-            <br/>
+            <br />
 
-            <div className="Blox">
-            <section className="Sub-Regiao">
-            <center>
-            <h3>Sub-Região</h3>
-            </center>
-            </section>
-            <p> {informacao.subRegiao}</p>
+            <div className="Blox-Informacoes">
+              <section className="Sub-Regiao-Titulo">
+                <center>
+                  <h4>Sub-Região</h4>
+                </center>
+              </section>
+              <p> {informacao.subRegiao}</p>
             </div>
-            </div>
-         
-            
-            </section>
-           
-
-
+          </div>
         </section>
+
+        <div className="Blox-Bandeira">
+          <img className="img1" src={informacao.bandeira}></img>
         </div>
-    )
+        <div />
+      </section>
+      <br />
+      <br />
+      <Footer />
+    </div>
+  );
 }
 
 export default RouteLocalStorage;
