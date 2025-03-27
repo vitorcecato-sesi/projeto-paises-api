ee7f66a7300228957bd593499acbde89c2d92604
Laura Betti <bettilaura0@gamail.com>
2025-03-27 07:55:42 -0300

    home fronteiras

--- src/pages/Page4.jsx
+++ src/pages/Page4.jsx
@@ -10,24 +10,32 @@ function Page4 () {
         <Navbar />
 
         <section className="bodyPage4"> 
-            <h2 className="titulo"> {informacoes.nomeComum} </h2>
+            
+            <section className="imgEBloco">
 
-            <section className="blocos">
-                <section className="bloco"> 
-                    <h3> Paises que Fazem Fronteira: </h3>
-                    <h3> <strong> {informacoes.paisesFronteira} </strong> </h3>
+                <section className="tituloBandeira"> 
+                    <h2 className="titulo"> {informacoes.nomeComum} </h2>
+                    <img className="imgBandeira" src={informacoes.bandeira}/>
                 </section>
 
-                <section className="bloco"> 
-                    <h3> Fuso-Horário: </h3>
-                    <h3> <strong> {informacoes.fusoHorario} </strong> </h3>
-                </section>
+                <section className="blocos">
+                    <section className="bloco"> 
+                        <h3> Paises que Fazem Fronteira: </h3>
+                        <h3> <strong> {informacoes.paisesFronteira} </strong> </h3>
+                    </section>
 
-                <section className="bloco"> 
-                    <h3> Sigla do País: </h3>
-                    <h3> <strong> {informacoes.sigla} </strong> </h3>
+                    <section className="bloco"> 
+                        <h3> Fuso-Horário: </h3>
+                        <h3> <strong> {informacoes.fusoHorario} </strong> </h3>
+                    </section>
+
+                    <section className="bloco"> 
+                        <h3> Sigla do País: </h3>
+                        <h3> <strong> {informacoes.sigla} </strong> </h3>
+                    </section>
                 </section>
             </section>
+
         </section>
         </>
     )
