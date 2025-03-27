1ec9ef57a64dc7d7400c1546f79116ee2d1adccd
Laura Betti <bettilaura0@gamail.com>
2025-03-27 08:29:06 -0300

    page4

--- src/pages/Page4.jsx
+++ src/pages/Page4.jsx
@@ -14,23 +14,23 @@ function Page4 () {
             <section className="imgEBloco">
 
                 <section className="tituloBandeira"> 
-                    <h2 className="titulo"> {informacoes.nomeComum} </h2>
+                    <h2 className="titulo"> 📍{informacoes.nomeComum} </h2>
                     <img className="imgBandeira" src={informacoes.bandeira}/>
                 </section>
 
                 <section className="blocos">
                     <section className="bloco"> 
-                        <h3> Paises que Fazem Fronteira: </h3>
+                        <h3> 🛫 Paises que Fazem Fronteira: </h3>
                         <h3> <strong> {informacoes.paisesFronteira} </strong> </h3>
                     </section>
 
                     <section className="bloco"> 
-                        <h3> Fuso-Horário: </h3>
+                        <h3> ⌚ Fuso-Horário: </h3>
                         <h3> <strong> {informacoes.fusoHorario} </strong> </h3>
                     </section>
 
                     <section className="bloco"> 
-                        <h3> Sigla do País: </h3>
+                        <h3> 🌏 Sigla do País: </h3>
                         <h3> <strong> {informacoes.sigla} </strong> </h3>
                     </section>
                 </section>
